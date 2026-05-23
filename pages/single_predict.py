4e879c9d0100da5a38c534b6341040146ec70869
nayreflection <nayreflection@gmail.com>
2026-05-23 23:39:34 +0700

    Update single_predict.py

--- pages/single_predict.py
+++ pages/single_predict.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+def show_single_predict():
+
+    st.title("Single Prediction")
+
+    text = st.text_area("Masukkan komentar")
+
+    if st.button("Analisis"):
+        st.success("Hasil sentimen muncul di sini")

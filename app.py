ed585bfdece18026907acf44d5b499d789c2f934
nayreflection <nayreflection@gmail.com>
2026-05-23 23:41:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,46 +3,53 @@ import streamlit as st
 # import halaman
 from pages.dashboard import show_dashboard
 from pages.single_predict import show_single_predict
-from pages.batch_predict import show_batch_predict
 from pages.statistics import show_statistics
+from pages.batch_predict import show_batch_predict
 from pages.about import show_about
 
-# ── CONFIG ─────────────────────────────
+# CONFIG
 st.set_page_config(
     page_title="News Sentiment Analysis",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
-# ── LOAD CSS ───────────────────────────
+# LOAD CSS
 with open("assets/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# ── SIDEBAR ────────────────────────────
-st.sidebar.title("📊 Sentiment Analysis")
+# SIDEBAR
+with st.sidebar:
 
-menu = st.sidebar.radio(
-    "Menu",
-    [
-        "Dashboard",
-        "Single Predict",
-        "Batch Predict",
-        "Statistics",
-        "About"
-    ]
-)
+    st.markdown("""
+    <div class="sidebar-title">
+        WEB MINING <br> SENTIMENT
+    </div>
+    """, unsafe_allow_html=True)
 
-# ── NAVIGASI ───────────────────────────
-if menu == "Dashboard":
+    page = st.selectbox(
+        "Menu",
+        [
+            "Dashboard",
+            "Analisis Sentimen",
+            "Statistik",
+            "Batch Prediksi",
+            "Tentang"
+        ]
+    )
+
+# ROUTING
+if page == "Dashboard":
     show_dashboard()
 
-elif menu == "Single Predict":
+elif page == "Analisis Sentimen":
     show_single_predict()
 
-elif menu == "Batch Predict":
-    show_batch_predict()
-
-elif menu == "Statistics":
+elif page == "Statistik":
     show_statistics()
 
-elif menu == "About":
+elif page == "Batch Prediksi":
+    show_batch_predict()
+
+elif page == "Tentang":
     show_about()
